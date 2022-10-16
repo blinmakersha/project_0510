5da37b46d7f14974198018d830ef38f33cbcf0d6
annashutova <annashut@inbox.ru>
2022-10-16 21:21:09 +0300

    Добавлена информация при выводе

--- team_main.py
+++ team_main.py
@@ -5,7 +5,9 @@ def calculate_two(eq: str):
     one, character, two = eq.split()
     return calculate(one, character, two)
 
-
+print('Добро пожаловать в супер-мега калькулятор!!!\n\
+    Введите математическое выражение по типу: 2 * 4')
 operation = input('Введите выражение: ')
 print('Ваш ответ: {0}'.format(calculate_two(operation)))
 print('Историю операций можете посмотреть в файле "calculator.txt"')
+
